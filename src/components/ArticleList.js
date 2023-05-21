2f14a9ba98540138f9f6aef99edc78cb00179a90
Jory Roberts <robertsjory13@gmail.com>
2023-05-21 13:14:26 -0500

    displays Header and About

---
+++ src/components/ArticleList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ArticleList = ({ post }) => {
+    return (
+        <div>
+            {/* <main>
+                {post.map((post) => {
+                    return (
+                        <Article
+                            key={post.id}
+                            date={post.date}
+                            preview={post.preview}
+                        ></Article>
+                    );
+                })}
+            </main> */}
+        </div>
+    );
+};
+
+export default ArticleList;
